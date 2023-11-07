5f5898b0ad776d7ce7584899db59ef6fd587407e
Jan Van den Schilden <jan.vandenschilden@gmail.com>
2023-11-07 14:08:35 +0100

    Add page for development of table

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
+import TabularTableDev from './ui/tabular-table-dev';
+
 
 ReactDOM.render(
     <BrowserRouter>
-        <Route component={App} />
+        <Route path="/app" component={App} />
+        <Route path="/dev" component={TabularTableDev} />
+
     </BrowserRouter>,
     document.getElementById('root')
 );
