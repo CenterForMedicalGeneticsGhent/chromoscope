5ddaef24c3ed3d9f7d9874759ba8961dd3461430
tsertijn <thomas.sertijn@ugent.be>
2024-09-05 10:09:14 +0200

    Update tsv_to_bed_bash.py

--- scripts/file_creation_for_higlassserver/tsv_to_bed_bash.py
+++ scripts/file_creation_for_higlassserver/tsv_to_bed_bash.py
@@ -13,6 +13,6 @@ df = pd.read_csv(filepath, delimiter="\t")
 if type_file.upper() == "BAF":
     df = df.drop(columns=['Sample'])
     df.columns = ['Chromosome', 'Start', 'End','REF', 'ALT','BAF']
-    df['Chromosome'] = 'chr' + df['Chromosome'].astype(str)
+    df['Chromosome'] = df['Chromosome'].apply(lambda x: f'chr{x}')
 bf = fuc.pybed.BedFrame.from_frame(meta=[], data=df)
 bf.to_file(f'{filename}.bed')
